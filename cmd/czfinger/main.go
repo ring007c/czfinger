0fd743edb168df7f6731bbdabdb620e7bdae9009
admin <bibei778@gmail.com>
2022-12-24 15:15:21 +0800

    11

--- cmd/czfinger/main.go
+++ cmd/czfinger/main.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	cli "github.com/jawher/mow.cli"
+	"os"
+)
+
+var (
+	app *cli.Cli
+)
+
+func main() {
+
+	app = cli.App("czfinger", "Simple website fingerprinting tool")
+	var (
+		target = app.StringOpt("t target", "", "Tager url")
+		//targetfile = app.StringOpt("tf targetfile", "", "Tager url file")
+		//threads    = app.IntOpt("t threads", 10, "threads nubmer ")
+		//timeout    = app.IntOpt("t timeout", 20, "timeout ")
+	)
+	app.Version("v version", "czfinger 1.0")
+	app.Spec = "-v | (-t=<target> )"
+	app.Action = func() {
+		targetsSlice := make([]string, 0) //创建一个目标地址切片
+		if len(*target) != 0 {
+			targetsSlice = append(targetsSlice, *target)
+		}
+		for _, v := range targetsSlice {
+		}
+
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		return
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module czfinger
 
 go 1.18
+
+require github.com/jawher/mow.cli v1.2.0
